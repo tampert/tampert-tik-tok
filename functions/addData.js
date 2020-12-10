48338ae5b3444c6bcb3798ae43d51e3e2e50de0b
tampert <d_soentjens@hotmail.com>
2020-12-10 17:48:14 +0100

    first commit

---
+++ functions/addData.js
@@ -0,0 +1,9 @@
+const { createClient } = require("@astrajs/collections");
+
+// create an Astra client
+const astraClient = await createClient({
+    astraDatabaseId: process.env.ASTRA_DB_ID,
+    astraDatabaseRegion: process.env.ASTRA_DB_REGION,
+    username: process.env.ASTRA_DB_USERNAME,
+    password: process.env.ASTRA_DB_PASSWORD,
+});
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Upload from './pages/Upload'
+
+import './index.css'
+
+const App = () => {
+  return (
+    <HashRouter>
+      <Header />
+      <Switch>
+        <Route path="/upload" component={Upload} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </HashRouter>
+  );
+}
+
+export default App;
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +28,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
